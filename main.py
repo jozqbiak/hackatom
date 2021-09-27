4c896c7230166c156afc7b889128d630bfd785dd
staschel <stas.chelkowski@gmail.com>
2021-09-27 16:33:32 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import numpy
 import pygame
 class Engine():
-	def __init__(self,m,ft,fg,x,v,dt):			#Mass, Force of Thrust, Force of Gravity, Starting position, starting velocity, delta time,
+	def __init__(self,m,ft,fg,x,v,dt,FU):			#Mass, Force of Thrust, Force of Gravity, Starting position, starting velocity, delta time, Fuel Usage
 		self.m = m
 		self.ft = ft
 		self.fg = fg
@@ -10,24 +10,20 @@ class Engine():
 		self.fin = False
 		self.dt = dt
 		self.timepassed = 0
-		self.maxa = ft/m
-
-	def start(self):
+		self.FU = FU
+	def _update_mass(self):							#Function updating mass by subtracting it's fuel usage multipied by delta time						
+		self.mass = self.mass - self.FU*self.dt
+		self.empty = 0
+	def _update_position_and_speed(self):
+		self.v = self.v + self.fg * self.dt + (ft/m)* self.dt
+		self.x = self.x + self.v * self.dt 		    		  #Function updating position and speed form the formulas
+	def _launch(self):
 		while self.fin == False:
-			self.v = self.v + self.fg*self.dt
-			self.x = self.x + self.v*self.dt
+			self._update_position_and_speed()
 			self.timepassed = self.timepassed + self.dt
 			print("x -",self.x,"v - (",self.v,")t -",self.timepassed)
-			if self.x < 0:
-				self.fin = True
-	def launch(self):
-		while self.fin == False:
-			self.v = self.v + self.fg * self.dt + self.maxa * self.dt
-			self.x = self.x + self.v * self.dt 
-			self.timepassed = self.timepassed + self.dt
-			print("x -",self.x,"v - (",self.v,")t -",self.timepassed)
-			if self.x > 2000 or self.timepassed > 10:
-				self.fin = True
+			if self.x > 2000:
+				self.fin = True														  #Testing function simulating rocket launch with maximal thrust until the rocket reaches 2000 m 
 
 m = 3000000 #Vessel mass in kg
 ft = 35000000 #Force thrust in N
@@ -35,5 +31,6 @@ fg = -9.8 #Force of gravity
 x = 0 #starting postion
 v = 0 #Starting velocity
 dt = 0.1 #Delta time in s 
-program = Engine(m,ft,fg,x,v,dt)
-program.launch()
+FU = 1000 #Fuel usage in kg/s
+program = Engine(m,ft,fg,x,v,dt,FU)
+program._launch()
