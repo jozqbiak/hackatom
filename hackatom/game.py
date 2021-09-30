cfbca78b60fd45fd23867d3c1f343467dc56ac3e
jozqbiak <jozex.kubiak@gmail.com>
2021-09-30 11:58:41 +0200

    moving ground

--- hackatom/game.py
+++ hackatom/game.py
@@ -1,106 +1,113 @@
-import pygame,sys
+import pygame, sys
 from main import Engine
 from random import randint
 from rakieta import Rakieta
 
-white= (255,255,255)
-red = (255,0,0)
-green = (0,200,0)
-blue = (135,206,235)
-black = (50,50,50)
-yellow = (255,255,0)
-m = 3000000 #Vessel mass in kg
-ft = 35000000 #Force thrust in N
-fg = -9.8 #Force of gravity
-x = 0 #starting postion
-v = 0 #Starting velocity
-dt = 0.02 #Delta time in s 
-FU = 1000 #Fuel usage in kg/s
+white = (255, 255, 255)
+red = (255, 0, 0)
+green = (0, 200, 0)
+blue = (135, 206, 235)
+black = (50, 50, 50)
+yellow = (255, 255, 0)
+m = 3000000  # Vessel mass in kg
+ft = 35000000  # Force thrust in N
+fg = -9.8  # Force of gravity
+x = 0  # starting postion
+v = 0  # Starting velocity
+dt = 0.02  # Delta time in s
+FU = 1000  # Fuel usage in kg/s
+
+
 class game():
-	def __init__(self, width, height):
-		self.engine = Engine(m,ft,fg,x,v,dt,FU)
+    def __init__(self, width, height):
+        self.engine = Engine(m, ft, fg, x, v, dt, FU)
 
-		self.width = width
-		self.height = height
-		self.size = self.width, self.height
-		self.scale = int(round(self.width*self.height/30000,0))
-		print(self.scale)
-		pygame.init()
-		self.screen = pygame.display.set_mode(self.size)
-		pygame.display.set_caption("Jebnie?")
-		self.BigKutafont = pygame.font.Font('penistypography.ttf',self.scale*3)
-		self.MediumKutafont = pygame.font.Font('penistypography.ttf',self.scale*2)
-		self.SmallKutafont = pygame.font.Font('penistypography.ttf',self.scale*1)
-		self.BigNumberFont = pygame.font.SysFont('Comic Sans MS',self.scale*3)
-		self.MediumNumberFont = pygame.font.SysFont('Comic Sans MS',self.scale*2)
-		self.SmallNumberFont = pygame.font.SysFont('Comic Sans MS',self.scale*1)
-		self.clock = pygame.time.Clock()
-		self.stopped = False
-		self.startsim = False
-		self.rakieta = Rakieta(self.width, self.height,self.screen)
-		self.time = 0
-		self.dt = dt
+        self.width = width
+        self.height = height
+        self.size = self.width, self.height
+        self.scale = int(round(self.width * self.height / 30000, 0))
+        print(self.scale)
+        pygame.init()
+        self.screen = pygame.display.set_mode(self.size)
+        pygame.display.set_caption("Jebnie?")
+        self.BigKutafont = pygame.font.Font('penistypography.ttf', self.scale * 3)
+        self.MediumKutafont = pygame.font.Font('penistypography.ttf', self.scale * 2)
+        self.SmallKutafont = pygame.font.Font('penistypography.ttf', self.scale * 1)
+        self.BigNumberFont = pygame.font.SysFont('Comic Sans MS', self.scale * 3)
+        self.MediumNumberFont = pygame.font.SysFont('Comic Sans MS', self.scale * 2)
+        self.SmallNumberFont = pygame.font.SysFont('Comic Sans MS', self.scale * 1)
+        self.ground = pygame.image.load("GROUND.png")
+        self.clock = pygame.time.Clock()
+        self.stopped = False
+        self.startsim = False
+        self.rakieta = Rakieta(self.width, self.height, self.screen)
+        self.time = 0
+        self.dt = dt
 
-	def _menu(self):
-		self.title = self.BigKutafont.render('Lecimy jak rakieta',True,white)
-		self.screen.blit(self.title,(self.width/2,50))
-	def _drawCloud(self): 
-		for i in range(1,16):  
-			self.size=randint(20,70)
-			self.xOffset=randint(-40,40)
-			self.yOffset=randint(-30,30)
-			pygame.draw.ellipse(self.screen,white,[100+self.xOffset,100+self.yOffset,50,50])		#100+xOffset,100+yOffset,size,size
-	def _height_index(self,value):
-		if value > 5000:
-			self.index = self.SmallNumberFont.render(str(round(value/1000,0))+ " k",True,white)
-		else:
-			self.index = self.SmallNumberFont.render(str(round(value,0)),True,white)
-		self.screen.blit(self.index,(0,10))
+    def _menu(self):
+        self.title = self.BigKutafont.render('Lecimy jak rakieta', True, white)
+        self.screen.blit(self.title, (self.width / 2, 50))
 
-	def _velocity_index(self,value):
-		self.index = self.SmallNumberFont.render(str(round(value,2)),True,white)
-		self.screen.blit(self.index,(0,30))
-	def _thrust_index(self,value):
-		self.index = self.SmallNumberFont.render(str(round(value,0)),True,white)
-		self.screen.blit(self.index,(0,50))
-	def _time_index(self,value):
-		self.index = self.SmallNumberFont.render(str(round(value,0)),True,white)
-		self.screen.blit(self.index,(0,70))
+    def _drawCloud(self):
+        for i in range(1, 16):
+            self.size = randint(20, 70)
+            self.xOffset = randint(-40, 40)
+            self.yOffset = randint(-30, 30)
+            pygame.draw.ellipse(self.screen, white,
+                                [100 + self.xOffset, 100 + self.yOffset, 50, 50])  # 100+xOffset,100+yOffset,size,size
 
-	def _rungame(self):
-		self.running = True
-		self._drawCloud()
-		while self.running:
-			self.time = self.time + self.dt
-			self.screen.fill(blue)
-			self.clock.tick(50)
-			self.rakieta._rotate_rakieta(40)
-			self.rakieta._blit_rakieta()
-			self._height_index(self.engine.x)
-			self._velocity_index(self.engine.v)
-			self._thrust_index(self.engine.ft)
-			self.keys = pygame.key.get_pressed()
-			self.engine._update_position_and_speed()
-			self.rakieta._check_rocket_status(self.engine.ft)
-			if self.startsim == False:
-				self._menu()
-			for event in pygame.event.get():
-				if event.type == pygame.QUIT:
-					self.running = False
-				if event.type == pygame.KEYUP:
-					if self.keys[pygame.K_UP]:
-						self.engine._change_ft(10000000)
-						print(1)
-				if self.keys[pygame.K_DOWN]:
-					self.engine._change_ft(-10000000)
+    def _height_index(self, value):
+        if value > 5000:
+            self.index = self.SmallNumberFont.render(str(round(value / 1000, 0)) + " k", True, white)
+        else:
+            self.index = self.SmallNumberFont.render(str(round(value, 0)), True, white)
+        self.screen.blit(self.index, (0, 10))
+
+    def _velocity_index(self, value):
+        self.index = self.SmallNumberFont.render(str(round(value, 2)), True, white)
+        self.screen.blit(self.index, (0, 30))
+
+    def _thrust_index(self, value):
+        self.index = self.SmallNumberFont.render(str(round(value, 0)), True, white)
+        self.screen.blit(self.index, (0, 50))
+
+    def _time_index(self, value):
+        self.index = self.SmallNumberFont.render(str(round(value, 0)), True, white)
+        self.screen.blit(self.index, (0, 70))
+
+    def _rungame(self):
+        self.running = True
+        self._drawCloud()
+        while self.running:
+            self.time = self.time + self.dt
+            self.screen.fill(blue)
+            self.screen.blit(self.ground, (500, 500 + self.engine.x))
+            self.screen.blit(self.ground, (-9500, 500 + self.engine.x))
+            self.clock.tick(50)
+            self.rakieta._rotate_rakieta(40)
+            self.rakieta._blit_rakieta()
+            self._height_index(self.engine.x)
+            self._velocity_index(self.engine.v)
+            self._thrust_index(self.engine.ft)
+            self.keys = pygame.key.get_pressed()
+            self.engine._update_position_and_speed()
+            self.rakieta._check_rocket_status(self.engine.ft)
+            if self.startsim == False:
+                self._menu()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+                if event.type == pygame.KEYUP:
+                    if self.keys[pygame.K_UP]:
+                        self.engine._change_ft(10000000)
+                        print(1)
+                if self.keys[pygame.K_DOWN]:
+                    self.engine._change_ft(-10000000)
+
+            pygame.display.flip()
+        pygame.quit()
 
 
-			pygame.display.flip()
-		pygame.quit()
-g = game(1000,600)
+g = game(1000, 600)
 g._rungame()
-#engine._launch()
-
-
-
-
+# engine._launch()
--- hackatom/rakieta.py
+++ hackatom/rakieta.py
@@ -1,45 +1,47 @@
-import pygame,sys
-white= (255,255,255)
-red = (255,0,0)
-green = (0,255,0)
-blue = (0,0,255)
-black = (0,0,0)
+import pygame
+
+white = (255, 255, 255)
+red = (255, 0, 0)
+green = (0, 255, 0)
+blue = (0, 0, 255)
+black = (0, 0, 0)
+
 
 class Rakieta(pygame.sprite.Sprite):
-	def __init__(self,width,height,screen):
-		self.width = width
-		self.height = height
-		self.screen = screen
-		pygame.sprite.Sprite.__init__(self)
-		self.rakieta = pygame.image.load('rakieta.png').convert_alpha()
-		self.left_roation = 0
-		self.rakieta_rect = self.rakieta.get_rect(center = (self.width/2, self.height-self.height/3.5))
-		print(self.rakieta_rect)
-	def _blit_rakieta(self):
-		self.screen.blit(self.rakieta,self.rakieta_rect)
-	def _rotate_rakieta(self,a):
-		pygame.transform.rotate(self.rakieta, a)
-		self.rakieta_rect = self.rakieta.get_rect(center = (self.width/2, self.height-self.height/3.5))
-	def _check_rocket_status(self,ft):
-		if ft <= 0:
-			self.rakieta = pygame.image.load('rakieta.png').convert_alpha()
-		else:
-			self.rakieta = pygame.image.load('rakietaogien.png').convert_alpha()
+    def __init__(self, width, height, screen):
+        self.width = width
+        self.height = height
+        self.screen = screen
+        pygame.sprite.Sprite.__init__(self)
+        self.rakieta = pygame.image.load('rakieta.png').convert_alpha()
+        self.left_roation = 0
+        self.rakieta_rect = self.rakieta.get_rect(center=(self.width / 2, self.height - self.height / 3.5))
+        print(self.rakieta_rect)
 
+    def _blit_rakieta(self):
+        # self.screen.blit(self.rakieta, self.rakieta_rect)
+        self.screen.blit(self.rakieta, [350, 215])
 
+    def _rotate_rakieta(self, a):
+        pygame.transform.rotate(self.rakieta, a)
+        self.rakieta_rect = self.rakieta.get_rect(center=(self.width / 2, self.height - self.height / 3.5))
 
+    def _check_rocket_status(self, ft):
+        if ft <= 0:
+            self.rakieta = pygame.image.load('rakieta.png').convert_alpha()
+        else:
+            self.rakieta = pygame.image.load('rakietaogien.png').convert_alpha()
 
-	def _turbulences(self):
-		if self.left_roation == 0:
-			self.left_roation =+1
-			pygame.transform.rotate(self.rakieta, -3)
-		elif self.left_roation == 1:
-			self.left_roation =+1
-			pygame.transform.rotate(self.rakieta, 3)
-		elif self.left_roation == 2:
-			self.left_roation =+1
-			pygame.transform.rotate(self.rakieta, 3)
-		elif self.left_roation == 3:
-			pygame.transform.rotate(self.rakieta, -3)
-			self.left_roation = 0
-
+    def _turbulences(self):
+        if self.left_roation == 0:
+            self.left_roation = +1
+            pygame.transform.rotate(self.rakieta, -3)
+        elif self.left_roation == 1:
+            self.left_roation = +1
+            pygame.transform.rotate(self.rakieta, 3)
+        elif self.left_roation == 2:
+            self.left_roation = +1
+            pygame.transform.rotate(self.rakieta, 3)
+        elif self.left_roation == 3:
+            pygame.transform.rotate(self.rakieta, -3)
+            self.left_roation = 0
